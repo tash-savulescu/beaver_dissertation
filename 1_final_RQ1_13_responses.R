3dbd674fe361c0e48e695537e25d555fc5be513c
tash-savulescu <138793803+tash-savulescu@users.noreply.github.com>
2023-08-27 17:01:03 +0100

    Update 1_final_RQ1_13_responses.R

--- 1_final_RQ1_13_responses.R
+++ 1_final_RQ1_13_responses.R
@@ -2,8 +2,15 @@
 #To identify the underlying determinants impacting landowners' WTA compensation, a generalised linear model (GLM) will be built.
 #By building a GLM, the degree of influence of each significant determinant can be explored.
 
+#Install all packages
 install.packages("MuMIn")
 library(MuMIn)
+install.packages("gplots") 
+library(gplots)
+install.packages("Hmisc")
+library(Hmisc)
+install.packages("corrplot")
+library(corrplot)
 
 #Load data set
 attach(X0_test_data_13_responses)
@@ -39,19 +46,7 @@ test_model <- subset(test_model, select = c(Land_Size, Land_Ownership, Total_Ind
 cor_matrix <- cor(test_model)
 
 
-# Install the gplots package 
-install.packages("gplots") 
-library(gplots)           
-
-
-# Calculate Significance values of variables
-
-# install packages 
-install.packages("Hmisc")
-library(Hmisc)
-install.packages("corrplot")
-library(corrplot)
-
+# Calculate significance values of variables
 test_model.rcorr = rcorr(as.matrix(test_model))
 test_model.rcorr
 
@@ -96,12 +91,12 @@ Emotional_WS <- test_histograms$Emotional_WS
 #Create a histogram
 ggplot(test_histograms, aes(x = Willingness_Score)) +
   geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
-  labs(title = "Histogram of Willingness to Accept Comepnsation", x = "WTA Compensation Value", y = "Frequency of Responses")
+  labs(title = "Histogram of Willingness Score", x = "Willingness Score", y = "Frequency of Responses")
 
 
 ggplot(test_graph, aes(x = Costs_WS)) +
   geom_histogram(binwidth = 1, fill = "pink", color = "black") +
-  labs(title = "Histogram of Factors Influencing Willingness to Accept Comepnsation", x = "Costs associated with mitigating beaver activity", y = "Frequency of Responses")
+  labs(title = "Histogram of Factors Influencing Willingness Score", x = "Costs associated with mitigating beaver activity", y = "Frequency of Responses")
 
 
 ggplot(test_graph, aes(x = Economic_Value_WS)) +
@@ -129,16 +124,16 @@ ggplot(test_graph, aes(x = Emotional_WS)) +
 
 
 #STEP 3: BUILDING LINEAR REGRESSION SCATTER PLOTS 
-test_model <- X0_test_data_13_responses
+glm_model <- X0_test_data_13_responses
 
 #Null ID column
-test_model$ID <- NULL
+glm_model$ID <- NULL
 
-test_model <- subset(test_model, select = c(Land_Size, Land_Ownership, Total_Indiv_Pos_Impact_Score, Total_Indiv_Neg_Impact_Score, Total_Com_Pos_Impact_Score, Total_Com_Neg_Impact_Score, Overall_Benefit_Individual, Overall_Benefit_Community, Willingness_Score, Economic_Value_WS, Costs_WS, Alternative_Solutions_WS, Ecological_Importance_WS, Emotional_WS))
+glm_model <- subset(glm_model , select = c(Land_Size, Land_Ownership, Total_Indiv_Pos_Impact_Score, Total_Indiv_Neg_Impact_Score, Total_Com_Pos_Impact_Score, Total_Com_Neg_Impact_Score, Overall_Benefit_Individual, Overall_Benefit_Community, Willingness_Score, Economic_Value_WS, Costs_WS, Alternative_Solutions_WS, Ecological_Importance_WS, Emotional_WS))
 
-# Sample data
-x <- test_model$Total_Indiv_Neg_Impact_Score
-y <- test_model$Willingness_Score
+# Plot a linear regression scatterplot of WS and Total Individual Negative Impact Score
+x <- glm_model$Total_Indiv_Neg_Impact_Score
+y <- glm_model$Willingness_Score
 data <- data.frame(x, y)
 
 # Calculate the correlation coefficient
@@ -154,23 +149,6 @@ ggplot(data, aes(x = x, y = y)) +
        y = "Willingness to Accept Compensation Score") +
   theme_minimal()
 
-# Sample data
-x <- test_model$Costs_WS
-y <- test_model$Willingness_Score
-data <- data.frame(x, y)
-
-# Calculate the correlation coefficient
-correlation_coefficient <- cor(data$x, data$y)
-print(paste("Correlation Coefficient: ", correlation_coefficient))
-
-# Scatter plot with a linear regression line
-ggplot(data, aes(x = x, y = y)) +
-  geom_point() +                       
-  geom_smooth(method = "lm", se = FALSE) +  
-  labs(title = "Scatter Plot with Linear Regression Line",
-       x = "Costs of Mitigating Beaver Activity Influencing WTA",
-       y = "Willingness to Accept Compensation Score") +
-  theme_minimal()
 
 # STEP 4: BUILDING GLM
 
@@ -181,29 +159,33 @@ library(car)
 
 # Step 4.1: Prepare the data with dependent variable (response) being WTA and explanatory variables
 
-Land_Ownership <- test_model$Land_Ownership
-Total_Indiv_Pos_Impact_Score <- test_model$Total_Indiv_Pos_Impact_Score
-Total_Indiv_Neg_Impact_Score <- test_model$Total_Indiv_Neg_Impact_Score
-Total_Com_Pos_Impact_Score <- test_model$Total_Com_Pos_Impact_Score
-Costs_WS <- test_model$Costs_WS
-Emotional_WS <- test_model$Emotional_WS
-Ecological_Importance_WS <- test_model$Ecological_Importance_WS 
+Land_Ownership <- glm_model$Land_Ownership
+Total_Indiv_Pos_Impact_Score <- glm_model$Total_Indiv_Pos_Impact_Score
+Total_Indiv_Neg_Impact_Score <- glm_model$Total_Indiv_Neg_Impact_Score
+Overall_Benefit_Individual <- glm_model$Overall_Benefit_Individual
+Emotional_WS <- glm_model$Emotional_WS
+Ecological_Importance_WS <- glm_model$Ecological_Importance_WS 
   
 
 # Step 4.2: Fit the GLM (assuming Gaussian family and identity link)
-model <- glm(Willingness_Score ~ Land_Ownership + Total_Indiv_Neg_Impact_Score + Total_Indiv_Pos_Impact_Score + Costs_WS + Emotional_WS + Ecological_Importance_WS, data = data, family = gaussian(link = "identity"))
+model <- glm(Willingness_Score ~ Land_Ownership + Total_Indiv_Neg_Impact_Score + 
+               Total_Indiv_Pos_Impact_Score + Overall_Benefit_Individual + Emotional_WS + 
+               Ecological_Importance_WS, data = glm_model, family = gaussian(link = "identity"))
 
 summary(model)
 
-# Step 4.3: Check for multi-collinearity by calculating the correlation matrix between explanatory variables
-cor_matrix <- cor(test_model [, c("Land_Ownership", "Total_Indiv_Neg_Impact_Score", "Total_Indiv_Pos_Impact_Score", "Costs_WS", "Emotional_WS", "Ecological_Importance_WS")])
+
+# Step 5: ADDITIONAL STASTICAL CHECKS FOR VALIDITY
+# Step 5.1: Check for multi-collinearity by calculating the correlation matrix between explanatory variables
+cor_matrix <- cor(test_model [, c("Land_Ownership", "Total_Indiv_Neg_Impact_Score", "Total_Indiv_Pos_Impact_Score",
+                                  "Costs_WS", "Emotional_WS", "Ecological_Importance_WS")])
 show(cor_matrix)
 
-# Calculate Variance Inflation Factor (VIF) for each predictor
+# Step 5.2: Calculate Variance Inflation Factor (VIF) for each predictor
 vif_values <- car::vif(model)
 summary(vif_values)
 
-# Step 4.4: Check for homoscedasticity by plotting residuals against predicted values
+# Step 5.3: Check for homoscedasticity by plotting residuals against predicted values
 residuals <- resid(model)
 fitted_values <- fitted(model)
 
@@ -213,7 +195,7 @@ ggplot(data.frame(residuals, fitted_values), aes(x = fitted_values, y = residual
   labs(title = "Residuals vs. Fitted Values", x = "Fitted Values", y = "Residuals") +
   theme_minimal()
 
-# Step 4.5: Check for multivariate normality by plotting histogram and Q-Q plot of model residuals
+# Step 5.4: Check for multivariate normality by plotting histogram and Q-Q plot of model residuals
 ggplot(data.frame(residuals), aes(x = residuals)) +
   geom_histogram(binwidth = 0.5) +
   labs(title = "Histogram of Residuals") +
@@ -223,19 +205,77 @@ qqnorm(residuals)
 qqline(residuals)
 
 
-# Step 5: ADDITIONAL STASTICAL CHECKS FOR VALIDITY
-# Step 5.1: Perform the Shapiro-Wilk test for normality
+# Step 5.6: Perform the Shapiro-Wilk test for normality
 shapiro.test(residuals)
 
-# Step 5.2: Perform power analysis
+
+# Step 6: Perform power analysis
+# Step 6.1: Install packages needed for power analysis
 install.packages("pwr")
 library(pwr)
 
+# Step 6.2: State the effect size, power and significance level
 effect_size <- 0.5
 alpha <- 0.05
 power <- 0.80
 
+# Step 6.3: Run the equation for the power analysis
 result <- pwr.t.test(d = effect_size, sig.level = alpha, power = power)
 
-# Print the required sample size
+# Step 6.4: Print the required sample size
 result$n
+
+# Step 6.5:Test to see the effect of different effect sizes
+# Create a range of effect sizes
+effect_sizes <- seq(0.2, 1.0, by = 0.1)
+
+# Perform power analysis for each effect size
+required_sample_sizes <- sapply(effect_sizes, function(d) {
+  pwr::pwr.t.test(d = d, sig.level = 0.05, power = 0.80)$n
+})
+
+# Create a data frame for plotting
+data <- data.frame(effect_size = effect_sizes, required_sample_size = required_sample_sizes)
+
+# Show the result of different effect sizes and the required sample size in a scatter plot
+ggplot(data, aes(x = effect_size, y = required_sample_size)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Effect size (Cohen's d) (d)",
+       y = "Required sample size (n)",
+       title = "Power analysis results : required sample size vs. effect size")
+
+
+
+
+
+# FURTHER EVALUATION OF ASSUMPTIONS THROUGH SENSE CHECKING OF OTHER MODELS 
+# Checking model with only one statistically significant variable 
+model_2 <- glm(Willingness_Score ~ Total_Indiv_Neg_Impact_Score, data = data, family = gaussian(link = "identity"))
+summary(model_2)
+
+# Fitting an ordinal GLM, therefore checking a different family link
+install.packages("ordinal")
+library(ordinal)
+install.packages("MASS")
+library(MASS)
+
+# Convert Willingness_Score to an ordered factor
+test_model$Willingness_Score <- factor(test_model$Willingness_Score, ordered = TRUE)
+
+# Specify the order of the levels if needed (e.g., for a 5-point Likert scale)
+test_model$Willingness_Score <- factor(test_model$Willingness_Score, 
+                                       ordered = TRUE, 
+                                       levels = c("1", "2", "3", "4", "5"))
+
+Willingness_Score <- test_model$Willingness_Score
+
+model_3 <- polr(Willingness_Score ~ Land_Ownership + Total_Indiv_Neg_Impact_Score + Total_Indiv_Pos_Impact_Score + Overall_Benefit_Individual + Emotional_WS + Ecological_Importance_WS, data = data, Hess = TRUE)
+
+# Print model summary
+summary(model_3)
+
+# Checking against a cumulative link model
+model_4 <- clm(Willingness_Score ~ Land_Ownership + Total_Indiv_Neg_Impact_Score + Total_Indiv_Pos_Impact_Score + Overall_Benefit_Individual + Emotional_WS + Ecological_Importance_WS, data = data)
+
+summary(model_4)
